98e3d094419ebb85f7f7ba83950dce07e8c939c5
Andrew Lee <andyrewlee@gmail.com>
2015-10-01 09:21:40 -0700

    Add posts

--- app/views/components/postsBox.jsx
+++ app/views/components/postsBox.jsx
@@ -9,22 +9,38 @@ var PostsBox = React.createClass({
     var newPosts = posts.concat([post]);
     this.setState({model: newPosts});
   },
+  handlePostDelete: function(id) {
+    console.log('PostAdmin wants to delete', id);
+    var posts = this.state.model;
+    for(var i = 0; i < this.state.model.length; i++) {
+      if(this.state.model[i].id == id) {
+        this.state.model.splice(i, 1);
+        break;
+      }
+    }
+    this.setState({model: posts});
+  },
   render: function() {
     return (
-      <div className='postBox'>
+      <div className='postsBox'>
         <h1>Posts</h1>
         <PostForm onPostSubmit={this.handlePostSubmit}/>
-        <PostsList data={this.state.model} />
+        <PostsList data={this.state.model} 
+                   handlePostDelete={this.handlePostDelete} />
       </div>
     );
   }
 });
 
 var PostForm = React.createClass({
+  getInitialState: function() {
+    return {id: 4}
+  },
   handleSubmit: function(e) {
     e.preventDefault();
     var content = React.findDOMNode(this.refs.content).value.trim();
-    this.props.onPostSubmit({id: 4, content: content, created_at: '2015-09-24 17:40:41'});
+    this.props.onPostSubmit({id: this.state.id, content: content, created_at: '2015-09-24 17:40:41'});
+    this.setState({id: this.state.id + 1});
     React.findDOMNode(this.refs.content).value = '';
   },
   render: function() {
@@ -38,14 +54,23 @@ var PostForm = React.createClass({
 });
 
 var PostsList = React.createClass({
+  handlePostDelete: function() {
+    this.props.handlePostDelete(this.props.id);
+  },
   render: function() {
+    var handlePostDelete = this.props.handlePostDelete;
     var postNodes = this.props.data.map(function(post) {
       return (
-        <Post key={post.id} created_at={post.created_at} content={post.content} />
+        <Post
+          key={post.id}
+          id={post.id}
+          created_at={post.created_at}
+          content={post.content}
+          handlePostDelete={handlePostDelete} />
       );
     });
     return (
-      <div className="postList">
+      <div className="postsList">
         {postNodes}
       </div>
     );
@@ -53,15 +78,38 @@ var PostsList = React.createClass({
 });
 
 var Post = React.createClass({
+  handlePostDelete: function() {
+    this.props.handlePostDelete(this.props.id);
+  },
   render: function() {
     return (
-      <div className="post">
+      <div className="postContent">
         <h2>{this.props.created_at}</h2>
         <div>{this.props.content}</div>
+        <PostAdmin
+          key={this.props.id}
+          id={this.props.id}
+          handlePostDelete={this.props.handlePostDelete} />
       </div>
     )
   }
 });
 
+var PostAdmin = React.createClass({
+  handleDelete: function() {
+    this.props.handlePostDelete(this.props.id);
+  },
+  handleEdit: function() {
+    console.log('Edit pressed', this.props.id);
+  },
+  render: function() {
+    return (
+      <div className="postAdmin">
+        <button onClick={this.handleDelete}>Delete</button>
+        <button onClick={this.handleEdit}>Edit</button>
+      </div>
+    );
+  }
+});
 
 module.exports = PostsBox;
